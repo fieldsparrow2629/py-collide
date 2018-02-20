b821b5a201118d4c461ffa0bcdde7766d5c69dde
Jon Cooper <joncoop@users.noreply.github.com>
2018-02-20 09:05:48 -0500

    Add files via upload

--- edge_detection.py
+++ edge_detection.py
@@ -28,11 +28,8 @@ BLACK = (0, 0, 0)
     
 # Make a block
 block =  [375, 275, 50, 50]
-
-block_vx = 0
-block_vy = 0
-
-block_speed = 5
+vel = [0, 0]
+speed = 5
 
 
 # Select test case
@@ -60,55 +57,58 @@ while not done:
     left = state[pygame.K_LEFT]
     right = state[pygame.K_RIGHT]
 
-    if up:
-        block_v_y = -block_speed
-    elif down:
-        block_v_y = block_speed
-    else:
-        block_v_y = 0
-        
     if left:
-        block_v_x = -block_speed
+        vel[0] = -speed
     elif right:
-        block_v_x = block_speed
+        vel[0] = speed
     else:
-        block_v_x = 0
+        vel[0] = 0
 
+    if up:
+        vel[1] = -speed
+    elif down:
+        vel[1] = speed
+    else:
+        vel[1] = 0
+        
         
     # Game logic (Check for collisions, update points, etc.)
     ''' move the block '''
-    block[0] += block_v_x
-    block[1] += block_v_y
+    block[0] += vel[0]
+    block[1] += vel[1]
 
     ''' get block edges (makes collision resolution easier to read) '''
-    top = block[1]
-    bottom = block[1] +block[3]
     left = block[0]
     right = block[0] + block[2]
+    top = block[1]
+    bottom = block[1] +block[3]
+
 
     if case == 1:
         ''' if the block is moved out of the window, nudge it back on. '''
+        if left < 0:
+            block[0] = 0
+        elif right > WIDTH:
+            block[0] = WIDTH - block[2]
+
         if top < 0:
             block[1] = 0
         elif bottom > HEIGHT:
             block[1] = HEIGHT - block[3]
 
-        if left < 0:
-            block[0] = 0
-        elif right > WIDTH:
-            block[0] = WIDTH - block[2]
     
     elif case == 2:
         ''' if the block is moved completely off of the window, reposition it on the other side '''
+        if left > WIDTH:
+            block[0] = 0 - block[2]
+        elif right < 0:
+            block[0] = WIDTH
+            
         if bottom < 0:
             block[1] = HEIGHT
         elif top > HEIGHT:
             block[1] = 0 - block[3]
 
-        if left > WIDTH:
-            block[0] = 0 - block[2]
-        elif right < 0:
-            block[0] = WIDTH
     
     # Drawing code (Describe the picture. It isn't actually drawn yet.)
     screen.fill(BLACK)
